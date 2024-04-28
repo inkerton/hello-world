aad98061516a5d6b31cddb4494f88654ff2ba162
Janvi <127668005+inkerton@users.noreply.github.com>
2024-04-28 17:39:05 +0530

    Update activity.py

--- activity.py
+++ activity.py
@@ -14,52 +14,55 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-"""HelloWorld Activity: A case study for developing an activity."""
+"""HelloWorld Activity: A case study for developing an activity. Ported version from GTK3 to GTK4."""
 
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 from gettext import gettext as _
 
 from sugar3.activity import activity
-from sugar3.graphics.toolbarbox import ToolbarBox
-from sugar3.activity.widgets import StopButton
-from sugar3.activity.widgets import ActivityToolbarButton
 
 
 class HelloWorldActivity(activity.Activity):
     """HelloWorldActivity class as specified in activity.info"""
 
     def __init__(self, handle):
-        """Set up the HelloWorld activity."""
+        """Set up the HelloWorld activity using GTK4 widgets."""
         activity.Activity.__init__(self, handle)
 
-        # we do not have collaboration features
-        # make the share option insensitive
         self.max_participants = 1
 
-        # toolbar with the new toolbar redesign
-        toolbar_box = ToolbarBox()
+        # Main window and box for layout
+        self.window = Gtk.Window(title="Hello World")
+        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
 
-        activity_button = ActivityToolbarButton(self)
-        toolbar_box.toolbar.insert(activity_button, 0)
-        activity_button.show()
+        activity_button = Gtk.Button(label=_("Activity"))
+        self.box.pack_start(activity_button, False, False, 0)
 
-        separator = Gtk.SeparatorToolItem()
-        separator.props.draw = False
-        separator.set_expand(True)
-        toolbar_box.toolbar.insert(separator, -1)
-        separator.show()
+        separator = Gtk.Separator(orientation=Gtk.Orientation.VERTICAL)
+        self.box.pack_start(separator, False, False, 0)
+        
+        stop_button = Gtk.Button(label=_("Stop"))
+        stop_button.connect("clicked", self.on_stop_clicked)  # Connecting to stop function
+        self.box.pack_start(stop_button, False, False, 0)
 
-        stop_button = StopButton(self)
-        toolbar_box.toolbar.insert(stop_button, -1)
-        stop_button.show()
+        label = Gtk.Label(_("Hello World!"))
+        self.box.pack_start(label, True, True, 0)  # Make label expandable
+
+        # Adding box to window and show all widgets
+        self.window.add(self.box)
+        self.window.show_all()
+
+    def on_stop_clicked(self, button):
+        # Implementing stop functionality (close window)
+        self.window.destroy()
 
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show()
 
-        # label with the text, make the string translatable
+        # making the string translatable
         label = Gtk.Label(_("Hello World!"))
         self.set_canvas(label)
         label.show()
